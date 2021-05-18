845ef3c666870ad7b3d2e170085560da0ed9b16f
Michael Arco <59952560+mikeme09@users.noreply.github.com>
2021-05-18 23:32:08 +0800

    Create food.py

---
+++ food.py
@@ -0,0 +1,19 @@
+from random import randint
+from turtle import Turtle
+
+
+X = randint(-280, 280)  # generating a random coordinate for X axis
+Y = randint(-280, 280)  # generating a random coordinate for Y axis
+image = "download.gif"
+
+
+class Food(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.color("blue")
+        self.penup()
+        self.goto(X, Y)
+        self.shapesize(stretch_wid=0.6, stretch_len=0.6)
+        print(f"Coordinate value of the food ({X}, {Y})\n")
